0684e652aed3e4e28a7dd37d7290815a1128fc98
T <2478385@cognizant.com>
2026-03-31 09:23:08 +0530

    added pom.xml

---
+++ src/main/java/com/grantserve/grantserve1/Grantserve1Application.java
@@ -0,0 +1,13 @@
+package com.grantserve.grantserve1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grantserve1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grantserve1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/grantserve/grantserve1/Grantserve1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grantserve.grantserve1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grantserve1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
