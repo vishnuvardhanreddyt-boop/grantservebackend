1034d1031d45e59a034cfb58266c38f250c835ac
T <2478385@cognizant.com>
2026-03-31 20:08:09 +0530

    added Researcher-ResearcherDocument files

---
+++ src/main/java/com/grantserve/grantserve1/entity/ResearcherDocument.java
@@ -0,0 +1,75 @@
+package com.grantserve.grantserve1.entity;
+
+
+import com.grantserve.grantserve1.enums.DocType;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class ResearcherDocument {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long documentID;
+
+    @ManyToOne
+    @JoinColumn(name = "researcher_id")
+    @JsonBackReference
+    private Researcher researcher;
+
+    @Enumerated(EnumType.STRING)
+    private DocType docType;
+
+    private String fileURI;
+    private java.time.LocalDateTime uploadedDate;
+    private String verificationStatus;
+
+    public Long getDocumentID() {
+        return documentID;
+    }
+
+    public void setDocumentID(Long documentID) {
+        this.documentID = documentID;
+    }
+
+    public Researcher getResearcher() {
+        return researcher;
+    }
+
+    public void setResearcher(Researcher researcher) {
+        this.researcher = researcher;
+    }
+
+    public DocType getDocType() {
+        return docType;
+    }
+
+    public void setDocType(DocType docType) {
+        this.docType = docType;
+    }
+
+    public String getFileURI() {
+        return fileURI;
+    }
+
+    public void setFileURI(String fileURI) {
+        this.fileURI = fileURI;
+    }
+
+    public LocalDateTime getUploadedDate() {
+        return uploadedDate;
+    }
+
+    public void setUploadedDate(LocalDateTime uploadedDate) {
+        this.uploadedDate = uploadedDate;
+    }
+
+    public String getVerificationStatus() {
+        return verificationStatus;
+    }
+
+    public void setVerificationStatus(String verificationStatus) {
+        this.verificationStatus = verificationStatus;
+    }
+}
