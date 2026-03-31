79c2f565fb80c2bda4c6ba2d2bb7d78d9024ee8f
Rushitha <2478345@cognizant.com>
2026-04-01 00:09:07 +0530

    GrantApplication Controller File is added

---
+++ src/main/java/com/grantserve/grantserve1/controller/GrantApplicationController.java
@@ -0,0 +1,55 @@
+package com.grantserve.grantserve1.controller;
+
+import com.grantserve.grantserve1.dto.GrantApplicationDto;
+import com.grantserve.grantserve1.entity.GrantApplication;
+import com.grantserve.grantserve1.service.IGrantApplicationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/GrantApplication")
+public class GrantApplicationController {
+
+    @Autowired
+    private IGrantApplicationService iGrantApplicationService;
+
+    @PostMapping("/createApplication")
+    public ResponseEntity<String> createApplication(@Valid @RequestBody GrantApplicationDto grantApplication){
+        return ResponseEntity.status(HttpStatus.CREATED).body(iGrantApplicationService.createApplication(grantApplication));
+    }
+    @GetMapping("/FetchGrantApplication/{id}")
+    public ResponseEntity<List<GrantApplication>> FetchGrantApplication(@PathVariable Long id){
+        return ResponseEntity.ok(iGrantApplicationService.FetchGrantApplication(id));
+    }
+    @DeleteMapping("/DeleteApplication/{id}")
+    public ResponseEntity<String> DeleteApplication(@PathVariable long id){
+        return ResponseEntity.status(HttpStatus.OK).body(iGrantApplicationService.DeleteApplication(id));
+    }
+    @GetMapping("getApplication/{id}")
+    public ResponseEntity<GrantApplication> getApplication(@PathVariable long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(iGrantApplicationService.getApplication(id));
+    }
+
+    @GetMapping("/search")
+    public List<GrantApplication> search(
+            @RequestParam(required = false) Long id,
+            @RequestParam(required = false) String title) {
+        return iGrantApplicationService.search(id, title);
+    }
+
+    @GetMapping("/ProgramGrantApplications/{programID}")
+    public ResponseEntity<List<GrantApplication>> fetchProgramGrantApplications(@PathVariable Long programID){
+        return
+                iGrantApplicationService.fetchProgramGrantApplications(programID)
+                        .map(ResponseEntity::ok)
+                        .orElse(ResponseEntity.notFound().build());
+
+    }
+
+}
+
